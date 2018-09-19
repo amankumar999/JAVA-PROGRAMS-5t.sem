c157261f447f7b1e469b7e8dbc6f9dde523aa12a
amankumar999 <35595299+amankumar999@users.noreply.github.com>
2018-09-19 11:56:08 +0530

    Add files via upload

---
+++ aman1.java
@@ -0,0 +1,14 @@
+class aman1 {
+   public static void main(String[] args) {    	
+      int number = 10;
+	 
+      if (number > 0) {
+         System.out.println("Number is positive.");
+      }
+      else {
+         System.out.println("Number is not positive.");
+      }
+ 
+      System.out.println("This statement is always executed.");
+   }
+}
